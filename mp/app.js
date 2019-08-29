17099b929771b10d1b11631bec1b09c71e7ee57a
爱过去的天 <1255575568@qq.com>
2019-08-29 08:39:29 +0800

    小程序初步整理 删除了首页的下面一个导航页，删除了首页的没用的view

---
+++ mp/app.js
@@ -0,0 +1,78 @@
+/*
+ * 
+ * WordPres版微信小程序
+ * author: jianbo
+ * organization: 守望轩  www.watch-life.net
+ * github:    https://github.com/iamxjb/winxin-app-watch-life.net
+ * 技术支持微信号：iamxjb
+ * Copyright (c) 2017 https://www.watch-life.net All rights reserved.
+ * 
+ */
+
+
+App({
+    
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    // 小程序主动更新
+    this.updateManager();
+
+    
+  },
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },/*小程序主动更新
+    */
+  updateManager() {
+    if (!wx.canIUse('getUpdateManager')) {
+      return false;
+    }
+    const updateManager = wx.getUpdateManager();
+    updateManager.onCheckForUpdate(function (res) {
+    });
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '有新版本',
+        content: '新版本已经准备好，即将重启',
+        showCancel: false,
+        success(res) {
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      });
+    });
+    updateManager.onUpdateFailed(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本下载失败',
+        showCancel: false
+      })
+    });
+  },
+  globalData: {
+    userInfo: null,
+    openid: '',
+    isGetUserInfo: false,
+    isGetOpenid: false
+
+  }
+})
---
+++ mp/utils/config.js
@@ -0,0 +1,68 @@
+/*
+ * 
+ * WordPres版微信小程序
+ * author: jianbo
+ * organization: 守望轩  www.watch-life.net
+ * github:    https://github.com/iamxjb/winxin-app-watch-life.net
+ * 技术支持微信号：iamxjb
+ * 开源协议：MIT
+ * Copyright (c) 2017 https://www.watch-life.net All rights reserved.
+ */
+
+
+
+//配置域名,域名只修改此处。
+//如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
+var DOMAIN = "www.watch-life.net";
+var MINAPPTYPE="0";//小程序的类型，如果是企业小程序请填：0 ，如果是个人小程序请填：1
+var WEBSITENAME="守望轩"; //网站名称
+var ABOUTID = 1136; //小程序“关于”页面的id,此id是wordpress网站"页面"的id,注意这个"页面"是wordpress的"页面"，不是"文章"
+var PAGECOUNT='10'; //每页文章数目
+//CATEGORIESID 指定为all或者指定具体的分类id，请选择其一。
+var CATEGORIESID='all';  //专题页显示全部的分类
+//var CATEGORIESID = '1,1059,98,416,189,374,6,463';//指定专题页显示的分类的id
+//INDEXLISTTYPE 指定为all或者指定具体的分类id，请选择其一。
+var INDEXLISTTYPE="all"; //首页显示所有分类
+//var INDEXLISTTYPE ="11" //指定首页显示分类的id
+var PAYTEMPPLATEID = 'hzKpxuPF2rw7O-qTElkeoE0lMwr0O4t9PJkLyt6v8rk';//鼓励消息模版id
+var REPLAYTEMPPLATEID = 'IiAVoBWP34u1uwt801rI_Crgen7Xl2lvAGP67ofJLo8';//回复评论消息模版id
+var ZANIMAGEURL = 'https://www.watch-life.net/images/2017/07/zsm400.jpg';//微信鼓励的图片链接，用于个人小程序的赞赏
+var LOGO = "../../images/logo-icon.png"; // 网站的logo图片
+//设置downloadFile合法域名,不带https ,在中括号([])里增加域名，格式：{id=**,domain:'www.**.com'}，用英文逗号分隔。
+//此处设置的域名和小程序与小程序后台设置的downloadFile合法域名要一致。
+var DOWNLOADFILEDOMAIN = [
+    { id: 1, domain: 'www.watch-life.net'}
+
+];
+ //首页图标导航
+ //参数说明：'name'为名称，'image'为图标路径，'url'为跳转的页面，'redirecttype'为跳转的类型，apppage为本小程序的页面，miniapp为其他微信小程序,webpage为web-view的页面
+ //redirecttype 是 miniapp 就是跳转其他小程序  url 为其他小程序的页面
+ //redirecttype 为 apppage 就是跳转本小程序的页面，url为本小程序的页面路径
+ //redirecttype 为 webpage 是跳转网址，是通过web-view打开网址，url就是你要打开的网址，不过这个网址的域名要是业务域名
+ //'appid' 当redirecttype为miniapp时，这个值为其他微信小程序的appid，如果redirecttype为apppage，webpage时，这个值设置为空。
+ //'extraData'当redirecttype为miniapp时，这个值为提交到其他微信小程序的参数，如果redirecttype为apppage，webpage时，这个值设置为空。
+var INDEXNAV = [
+  { id: '1', name: '微慕', image: '../../images/nav-icon1.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wx074a216a7aabc30c', extraData: '' },
+  { id: '2', name: '企业版', image: '../../images/nav-icon2.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wxc1771b619b83316b', extraData: '' },
+  { id: '3', name: '旅游版', image: '../../images/nav-icon3.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wxa43aaa8416cdbd61', extraData: '' },
+  { id: '4', name: '图片版', image: '../../images/nav-icon4.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wxe9440e3d24b04152', extraData: ''},
+  { id: '5', name: '微慕+', image: '../../images/nav-icon5.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wx46926513e9b706d3', extraData: ''}
+ 
+    
+    ];
+
+export default {
+  getDomain: DOMAIN,
+  getWebsiteName: WEBSITENAME,
+  getAboutId: ABOUTID,
+  getPayTemplateId: PAYTEMPPLATEID,
+  getPageCount: PAGECOUNT,
+  getCategoriesID :CATEGORIESID,
+  getIndexNav: INDEXNAV,
+  getReplayTemplateId: REPLAYTEMPPLATEID,
+  getMinAppType: MINAPPTYPE,
+  getZanImageUrl: ZANIMAGEURL,
+  getIndexListType: INDEXLISTTYPE,
+  getLogo: LOGO,
+  getDownloadFileDomain: DOWNLOADFILEDOMAIN
+}
