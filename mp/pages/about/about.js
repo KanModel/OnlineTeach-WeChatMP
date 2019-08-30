27f5b6eb0044c01b18fc8dd65527eb16b55a18b1
爱过去的天 <1255575568@qq.com>
2019-08-30 09:28:43 +0800

    微信小程序 关于导航栏样式

--- mp/pages/about/about.js
+++ mp/pages/about/about.js
@@ -22,7 +22,9 @@ var app = getApp();
 
 Page({
   data: {
-    title: '页面内容',
+      textIndex:'随着技术的发展，学习方式也逐渐从传统的教室现场教学转变为以网络为媒介的远程教学，如今随着互联网的发展，远程教学越来越普及，人们可以做到坐在电脑前进行学习和实时下载资料并与教师进行互动，大大增强了学习的效率。我们希望通过利用微信小程序平台实现搭建一个通过观看相关教学视频帮助用户学习软件编程的学习平台。',
+      gitHref:'https://github.com/KanModel/OnlineTeach-WeChatMP',
+      title: '页面内容',
     pageData: {},
     pagesList: {},
     display: 'none',
@@ -37,13 +39,11 @@ Page({
     isLoginPopup: false,
     openid:"",
     system:""
-   
-    
   },
   onLoad: function (options) {
     var self = this;
     wx.setNavigationBarTitle({
-      title: '关于WordPress微信小程序',
+      title: '关于移动智能学习平台',
       success: function (res) {
         // success
       }
@@ -91,8 +91,9 @@ Page({
           pageData:{},
           wxParseData:{},
 
-      });
 
+      });
+      console.log(wxParseData);
       this.fetchData(config.getAboutId);
       //消除下刷新出现空白矩形的问题。
       wx.stopPullDownRefresh()
