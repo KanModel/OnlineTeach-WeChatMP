6acffcc834e53c60ceea7c25c9f060fc8860351b
爱过去的天 <1255575568@qq.com>
2019-09-04 10:18:37 +0800

    刪除about detail index readlog list 页面无用代码和变量

--- mp/pages/list/list.js
+++ mp/pages/list/list.js
@@ -1,27 +1,10 @@
-/*
- * 
- * WordPres版微信小程序
- * author: jianbo
- * organization: 守望轩  www.watch-life.net
- * github:    https://github.com/iamxjb/winxin-app-watch-life.net
- * 技术支持微信号：iamxjb
- * 开源协议：MIT
- * 
- *  *Copyright (c) 2017 https://www.watch-life.net All rights reserved.
- */
-
+/* WordPres版微信小程序 author: jianbo organization: 守望轩  www.watch-life.net github:    https://github.com/iamxjb/winxin-app-watch-life.net 技术支持微信号：iamxjb 开源协议：MIT *Copyright (c) 2017 https://www.watch-life.net All rights reserved. */
 var Api = require('../../utils/api.js');
-var util = require('../../utils/util.js');
-var WxParse = require('../../wxParse/wxParse.js');
-var wxApi = require('../../utils/wxApi.js')
 var wxRequest = require('../../utils/wxRequest.js')
-
 import config from '../../utils/config.js'
 
 var pageCount = config.getPageCount;
-
 var app = getApp()
-
 Page({
     data: {
         title: '文章列表',
@@ -42,217 +25,101 @@ Page({
         displaySwiper: "block",
         floatDisplay: "none",
         searchKey: "",
-    },
-    formSubmit: function (e) {
-        var url = '../list/list'
-        if (e.detail.value.input != '') {
-            url = url + '?search=' + e.detail.value.input;
-        }
-        wx.navigateTo({
-            url: url
-        })
-    },
-    onShareAppMessage: function () {
-
-        var title = "分享“守望轩”";
+    }, onShareAppMessage: function () {
+        var title = "分享“袋袋云”";
         var path = ""
-
         if (this.data.categories && this.data.categories != 0) {
             title += "的专题：" + this.data.categoriesList.name;
             path = 'pages/list/list?categoryID=' + this.data.categoriesList.id;
-
         } else {
             title += "的搜索内容：" + this.data.searchKey;
             path = 'pages/list/list?search=' + this.data.searchKey;
         }
-
-
         return {
-            title: title,
-            path: path,
-            success: function (res) {
-                // 转发成功
-            },
-            fail: function (res) {
-                // 转发失败
+            title: title, path: path, success: function (res) {/* 转发成功*/
+            }, fail: function (res) {/* 转发失败*/
             }
         }
-    },
-    onReachBottom: function () {
-
+    }, onReachBottom: function () {
         var self = this;
         if (!self.data.isLastPage) {
-            self.setData({
-                page: self.data.page + 1
-            });
+            self.setData({page: self.data.page + 1});
             console.log('当前页' + self.data.page);
             this.fetchPostsData(self.data);
         } else {
             console.log('最后一页');
         }
-
     },
-    reload: function (e) {
-        var self = this;
-        if (self.data.categories && self.data.categories != 0) {
-
-            self.setData({
-                // categories: options.categoryID,
-                isCategoryPage: "block",
-                showallDisplay: "none",
-                showerror: "none",
-
-            });
-            self.fetchCategoriesData(self.data.categories);
-        }
-        if (self.data.search && self.data.search != '') {
-            self.setData({
-                //search: options.search,
-                isSearchPage: "block",
-                showallDisplay: "none",
-                showerror: "none",
-                searchKey: self.data.search
-            })
-        }
-        self.fetchPostsData(self.data);
-    },
-    //加载分页
-    loadMore: function (e) {
+    /*加载分页*/ loadMore: function (e) {
         var self = this;
         if (!self.data.isLastPage) {
-            self.setData({
-                page: self.data.page + 1
-            });
+            self.setData({page: self.data.page + 1});
             console.log('当前页' + self.data.page);
             this.fetchPostsData(self.data);
         } else {
-            wx.showToast({
-                title: '没有更多内容',
-                mask: false,
-                duration: 1000
-            });
+            wx.showToast({title: '没有更多内容', mask: false, duration: 1000});
         }
-    },
-    onLoad: function (options) {
+    }, onLoad: function (options) {
         var self = this;
         if (options.search && options.search != '') {
             wx.setNavigationBarTitle({
-                title: "搜索关键字：" + options.search,
-                success: function (res) {
-                    // success
+                title: "搜索关键字：" + options.search, success: function (res) {/* success*/
                 }
             });
-            self.setData({
-                search: options.search,
-                isSearchPage: "block",
-                searchKey: options.search
-            })
-
+            self.setData({search: options.search, isSearchPage: "block", searchKey: options.search})
             this.fetchPostsData(self.data);
         }
-    },
-    //获取文章列表数据
-    fetchPostsData: function (data) {
-
+    }, /*获取文章列表数据*/ fetchPostsData: function (data) {
         var self = this;
         if (!data) data = {};
         if (!data.page) data.page = 1;
-        // if (!data.categories) data.categories = 0;
-        // if (!data.search) data.search = '';
+        /* if (!data.categories) data.categories = 0; if (!data.search) data.search = '';*/
         if (data.page === 1) {
-            self.setData({
-                postsList: []
-            });
+            self.setData({postsList: []});
         }
-        ;
-        wx.showLoading({
-            title: '正在加载',
-            mask: true
-        });
+        ;wx.showLoading({title: '正在加载', mask: true});
         console.log(data)
         wx.request({
             url: app.globalData.url + 'post/list',
             method: "GET",
-            header: {
-                'content-type': 'application/x-www-form-urlencoded'
-            },
-            data: {
-                no : data.page,
-                size : pageCount,
-                title: data.search
-            },
+            header: {'content-type': 'application/x-www-form-urlencoded'},
+            data: {no: data.page, size: pageCount, title: data.search},
             success: function (result) {
                 console.log(result);
-                self.setData({
-                    floatDisplay: "block",
-                    postsList: self.data.postsList.concat(result.data)
-                });
+                self.setData({floatDisplay: "block", postsList: self.data.postsList.concat(result.data)});
                 setTimeout(function () {
                     wx.hideLoading();
                 }, 900);
-                if(result.data.length < pageCount)
-                {
-                    self.setData({
-                        isLastPage: true
-                    });
-                    wx.showToast({
-                        title: '没有更多内容',
-                        mask: false,
-                        duration: 1500
-                    });
+                if (result.data.length < pageCount) {
+                    self.setData({isLastPage: true});
+                    wx.showToast({title: '没有更多内容', mask: false, duration: 1500});
                 }
             }
         })
-    },
-
-
-    // 跳转至查看文章详情
-    redictDetail: function (e) {
-        // console.log('查看文章');
-        var id = e.currentTarget.id,
-            url = '../detail/detail?id=' + id;
-        wx.navigateTo({
-            url: url
-        })
-    },
-
-    //获取分类列表
-    fetchCategoriesData: function (id) {
+    }, /* 跳转至查看文章详情*/ redictDetail: function (e) {/* console.log('查看文章');*/
+        var id = e.currentTarget.id, url = '../detail/detail?id=' + id;
+        wx.navigateTo({url: url})
+    }, /*获取分类列表*/ fetchCategoriesData: function (id) {
         var self = this;
-        self.setData({
-            categoriesList: []
-        });
-
+        self.setData({categoriesList: []});
         var getCategoryRequest = wxRequest.getRequest(Api.getCategoryByID(id));
-
         getCategoryRequest.then(response => {
-
             var catImage = "";
             if (typeof (response.data.category_thumbnail_image) == "undefined" || response.data.category_thumbnail_image == "") {
                 catImage = "../../images/website.png";
             } else {
                 catImage = response.data.category_thumbnail_image;
             }
-
             self.setData({
                 categoriesList: response.data,
                 categoriesImage: catImage,
                 categoriesName: response.data.name
             });
-
             wx.setNavigationBarTitle({
-                title: response.data.name,
-                success: function (res) {
-                    // success
+                title: response.data.name, success: function (res) {/* success*/
                 }
             });
-
             self.fetchPostsData(self.data);
-
         })
     },
-
 })
-
-
-
--- mp/pages/readlog/readlog.js
+++ mp/pages/readlog/readlog.js
@@ -1,214 +1,80 @@
-/*
-*
-* WordPres版微信小程序
-* author: jianbo
-* organization: 守望轩  www.watch-life.net
-* github:    https://github.com/iamxjb/winxin-app-watch-life.net
-* 技术支持微信号：iamxjb
-* 开源协议：MIT
-*
-*  *Copyright (c) 2017 https://www.watch-life.net All rights reserved.
-*/
+/* WordPres版微信小程序 author: jianbo organization: 守望轩  www.watch-life.net github:    https://github.com/iamxjb/winxin-app-watch-life.net 技术支持微信号：iamxjb 开源协议：MIT *Copyright (c) 2017 https://www.watch-life.net All rights reserved. */
 import config from '../../utils/config.js'
 
-var Api = require('../../utils/api.js');
-var util = require('../../utils/util.js');
-var Auth = require('../../utils/auth.js');
-var WxParse = require('../../wxParse/wxParse.js');
-var wxApi = require('../../utils/wxApi.js')
-var wxRequest = require('../../utils/wxRequest.js');
 var app = getApp();
 Page({
-
     data: {
         postsList: [],
-        br: "\n",
         userInfo: {},
         hasUserInfo: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        readLogs: [],
-        tab: '1',
         showerror: "none",
         shownodata: "none",
-        subscription: "",
-        userLevel: {},
         openid: '',
         isLoginPopup: false
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+    }, /** 生命周期函数--监听页面加载 */ onLoad: function (options) {
         console.log(app.globalData.userInfo)
-    },
-    onShow:function () {
+    }, onShow: function () {
         console.log(app.globalData.userInfo)
         this.resetData();
-    },
-    resetData(){
+    }, resetData() {
         var that = this;
-        console.log('resetData' ,that.data.hasUserInfo);
+        console.log('resetData', that.data.hasUserInfo);
         wx.request({
             url: app.globalData.url + 'fav/list',
             data: {openid: app.globalData.openid, sig: app.globalData.sig},
-            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-            header: {
-                'content-type': 'application/x-www-form-urlencoded'
-            },// 设置请求的 header
+            method: 'GET', /* OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT*/
+            header: {'content-type': 'application/x-www-form-urlencoded'}, /* 设置请求的 header*/
             success: function (res) {
                 console.log(res.data)
-                that.setData({
-                    postsList: res.data,
-                    hasUserInfo:true,
-                    userInfo:app.globalData.userInfo,
-                });
+                that.setData({postsList: res.data, hasUserInfo: true, userInfo: app.globalData.userInfo,});
             },
             fail: function () {
                 console.log("index.js wx.request CheckCallUser fail");
             },
-            complete: function () {
-                // complete
-            }
         })
-    },
-    deleteFavorite: function (e) {
-        var that = this
-        console.log(e)
+    }, deleteFavorite: function (e) {
+        var that = this;
+        console.log(e);
         wx.request({
             url: app.globalData.url + 'fav/' + e.currentTarget.id,
             data: {openid: app.globalData.openid, sig: app.globalData.sig, postid: this.data.id},
-            method: 'DELETE', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-            header: {
-                'content-type': 'application/x-www-form-urlencoded'
-            },// 设置请求的 header
+            method: 'DELETE', /* OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT*/
+            header: {'content-type': 'application/x-www-form-urlencoded'}, /* 设置请求的 header*/
             success: function (res) {
                 console.log(res.statusCode)
                 if (res.statusCode == 200 || res.statusCode == 202) {
-                    wx.showToast({
-                        title: '取消成功',
-                        icon: 'success',
-                        duration: 2000
-                    })
-
+                    wx.showToast({title: '取消成功', icon: 'success', duration: 2000})
                 } else if (res.statusCode == 409) {
                     console.log("index.js wx.request CheckCallUser statusCode" + res.statusCode);
-                    wx.showToast({
-                        title: '重复添加',
-                        icon: 'none',
-                        duration: 2000
-                    })
+                    wx.showToast({title: '重复添加', icon: 'none', duration: 2000})
                 }
                 that.resetData()
             },
             fail: function () {
                 console.log("index.js wx.request CheckCallUser fail");
             },
-            complete: function () {
-
-            }
         })
-
-        // this.onShow()
-    },
-    getUserInfo: function (e) {
-        // console.log(e)
+    }, getUserInfo: function (e) {/* console.log(e)*/
         var that = this;
-        app.globalData.userInfo = e.detail.userInfo
-        this.setData({
-            userInfo: e.detail.userInfo,
-            hasUserInfo: true
-        })
+        console.log(e);
+        app.globalData.userInfo = e.detail.userInfo;
+        this.setData({userInfo: e.detail.userInfo, hasUserInfo: true})
         console.log(app.globalData.openid, app.globalData.sig);
         wx.request({
             url: app.globalData.url + 'fav/list',
             data: {openid: app.globalData.openid, sig: app.globalData.sig},
-            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-            header: {
-                'content-type': 'application/x-www-form-urlencoded'
-            },// 设置请求的 header
+            method: 'GET', /* OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT*/
+            header: {'content-type': 'application/x-www-form-urlencoded'}, /* 设置请求的 header*/
             success: function (res) {
                 console.log(res.data)
-                that.setData({
-                    postsList: res.data
-                });
+                that.setData({postsList: res.data});
             },
             fail: function () {
                 console.log("index.js wx.request CheckCallUser fail");
             },
-            complete: function () {
-                // complete
-            }
         })
-    },
-    onReady: function () {
-        var self = this;
-        Auth.checkSession(self, 'isLoginNow');
-    },
-    agreeGetUser: function (e) {
-        let self = this;
-        Auth.checkAgreeGetUser(e, app, self, '0');
-
-    },
-
-    refresh: function (e) {
-        var self = this;
-        if (self.data.openid) {
-            var args = {};
-            var userInfo = e.detail.userInfo;
-            args.openid = self.data.openid;
-            args.avatarUrl = userInfo.avatarUrl;
-            args.nickname = userInfo.nickName;
-            var url = Api.getUpdateUserInfo();
-            var postUpdateUserInfoRequest = wxRequest.postRequest(url, args);
-            postUpdateUserInfoRequest.then(res => {
-                if (res.data.status == '200') {
-                    var userLevel = res.data.userLevel;
-                    wx.setStorageSync('userInfo', userInfo);
-                    wx.setStorageSync('userLevel', userLevel);
-                    self.setData({userInfo: userInfo});
-                    self.setData({userLevel: userLevel});
-                    wx.showToast({
-                        title: res.data.message,
-                        icon: 'success',
-                        duration: 900,
-                        success: function () {
-                        }
-                    })
-                } else {
-                    wx.showToast({
-                        title: res.data.message,
-                        icon: 'success',
-                        duration: 900,
-                        success: function () {
-                        }
-                    })
-                }
-
-
-            });
-        } else {
-            Auth.checkSession(self, 'isLoginNow');
-
-        }
-
-    },
-
-    exit: function (e) {
-
-        Auth.logout(this);
-        wx.reLaunch({
-            url: '../index/index'
-        })
-    },
-    clear: function (e) {
-
-        Auth.logout(this);
-
-    },
-
-    // 跳转至查看文章详情
-    redictDetail: function (e) {
+    }, redictDetail: function (e) {
         var id = e.currentTarget.id;
         var itemtype = e.currentTarget.dataset.itemtype;
         var url = "";
@@ -216,256 +82,7 @@ Page({
             url = '../list/list?categoryID=' + id;
         } else {
             url = '../detail/detail?id=' + id;
-
         }
-
-        wx.navigateTo({
-            url: url
-        })
+        wx.navigateTo({url: url})
     },
-    onTapTag: function (e) {
-        var self = this;
-        var tab = e.currentTarget.id;
-        var topBarItems = self.data.topBarItems;
-        // 切换topBarItem
-        for (var i = 0; i < topBarItems.length; i++) {
-            if (tab == topBarItems[i].id) {
-                topBarItems[i].selected = true;
-            } else {
-                topBarItems[i].selected = false;
-            }
-        }
-        self.setData({
-            topBarItems: topBarItems,
-            tab: tab
-
-        })
-        if (tab !== 0) {
-            this.fetchPostsData(tab);
-        } else {
-            this.fetchPostsData("1");
-        }
-    },
-    onShareAppMessage: function () {
-        var title = "分享我在“" + config.getWebsiteName + "浏览、评论、点赞、鼓励的文章";
-        var path = "pages/readlog/readlog";
-        return {
-            title: title,
-            path: path,
-            success: function (res) {
-                // 转发成功
-            },
-            fail: function (res) {
-                // 转发失败
-            }
-        }
-    },
-    fetchPostsData: function (tab) {
-        self = this;
-        self.setData({
-            showerror: 'none',
-            shownodata: 'none'
-        });
-        var count = 0;
-        var openid = "";
-        if (tab != '1') {
-            if (self.data.openid) {
-                var openid = self.data.openid;
-            } else {
-                Auth.checkSession(self, 'isLoginNow');
-                return;
-            }
-
-
-        }
-        if (tab == '1') {
-            self.setData({
-                readLogs: (wx.getStorageSync('readLogs') || []).map(function (log) {
-                    count++;
-                    return log;
-                })
-            });
-            if (count == 0) {
-                self.setData({
-                    shownodata: 'block'
-                });
-            }
-        } else if (tab == '2') {
-            self.setData({
-                readLogs: []
-            });
-            var getMyCommentsPosts = wxRequest.getRequest(Api.getWeixinComment(openid));
-            getMyCommentsPosts.then(response => {
-                if (response.statusCode == 200) {
-                    self.setData({
-                        readLogs: self.data.readLogs.concat(response.data.data.map(function (item) {
-                            count++;
-                            item[0] = item.post_id;
-                            item[1] = item.post_title;
-                            return item;
-                        }))
-                    });
-                    if (count == 0) {
-                        self.setData({
-                            shownodata: 'block'
-                        });
-                    }
-                } else {
-                    console.log(response);
-                    self.setData({
-                        showerror: 'block'
-                    });
-
-                }
-            })
-        } else if (tab == '3') {
-            self.setData({
-                readLogs: []
-            });
-            var getMylikePosts = wxRequest.getRequest(Api.getMyLikeUrl(openid));
-            getMylikePosts.then(response => {
-                if (response.statusCode == 200) {
-                    self.setData({
-                        readLogs: self.data.readLogs.concat(response.data.data.map(function (item) {
-                            count++;
-                            item[0] = item.post_id;
-                            item[1] = item.post_title;
-                            item[2] = "0";
-                            return item;
-                        }))
-                    });
-
-                    if (count == 0) {
-                        self.setData({
-                            shownodata: 'block'
-                        });
-                    }
-                } else {
-                    console.log(response);
-                    self.setData({
-                        showerror: 'block'
-                    });
-
-                }
-            })
-
-        } else if (tab == '4') {
-            self.setData({
-                readLogs: []
-            });
-
-            var getMyPraisePosts = wxRequest.getRequest(Api.getMyPraiseUrl(openid));
-            getMyPraisePosts.then(response => {
-                if (response.statusCode == 200) {
-                    self.setData({
-                        readLogs: self.data.readLogs.concat(response.data.data.map(function (item) {
-                            count++;
-                            item[0] = item.post_id;
-                            item[1] = item.post_title;
-                            item[2] = "0";
-                            return item;
-                        }))
-                    });
-                    if (count == 0) {
-                        self.setData({
-                            shownodata: 'block'
-                        });
-                    }
-                } else {
-                    console.log(response);
-                    this.setData({
-                        showerror: 'block'
-                    });
-
-                }
-            })
-
-        } else if (tab == '5') {
-            self.setData({
-                readLogs: []
-            });
-            var url = Api.getSubscription() + '?openid=' + openid;
-            var getMysubPost = wxRequest.getRequest(url);
-            getMysubPost.then(response => {
-                if (response.statusCode == 200) {
-                    var usermetaList = response.data.usermetaList;
-                    if (usermetaList) {
-                        self.setData({
-                            readLogs: self.data.readLogs.concat(usermetaList.map(function (item) {
-                                count++;
-                                item[0] = item.ID;
-                                item[1] = item.post_title;
-                                item[2] = "0";
-                                return item;
-                            }))
-                        });
-
-                    }
-                    if (count == 0) {
-                        self.setData({
-                            shownodata: 'block'
-                        });
-                    }
-                } else {
-                    console.log(response);
-                    this.setData({
-                        showerror: 'block'
-                    });
-
-                }
-            })
-
-
-        } else if (tab == '6') {
-            self.setData({
-                readLogs: []
-            });
-            var getNewComments = wxRequest.getRequest(Api.getNewComments());
-            getNewComments.then(response => {
-                if (response.statusCode == 200) {
-                    self.setData({
-                        readLogs: self.data.readLogs.concat(response.data.map(function (item) {
-                            count++;
-                            item[0] = item.post;
-                            item[1] = util.removeHTML(item.content.rendered + '(' + item.author_name + ')');
-                            item[2] = "0";
-                            return item;
-                        }))
-                    });
-                    if (count == 0) {
-                        self.setData({
-                            shownodata: 'block'
-                        });
-                    }
-
-                } else {
-                    console.log(response);
-                    self.setData({
-                        showerror: 'block'
-                    });
-
-                }
-            }).catch(function () {
-                console.log(response);
-                self.setData({
-                    showerror: 'block'
-                });
-
-            })
-        }
-    },
-    closeLoginPopup() {
-        this.setData({isLoginPopup: false});
-    },
-    openLoginPopup() {
-        this.setData({isLoginPopup: true});
-    }
-    ,
-    confirm: function () {
-        this.setData({
-            'dialog.hidden': true,
-            'dialog.title': '',
-            'dialog.content': ''
-        })
-    }
 })
