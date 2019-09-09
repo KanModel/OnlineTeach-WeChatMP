efbfdf581771704b1b890845782afe8cbf61029b
KanModel <kgdwhsk@gmail.com>
2019-09-09 09:00:39 +0800

    修正Swagger中配置REST包位置

--- src/main/java/me/kanmodel/july19/onlineteach/adapter/SwaggerConfig.java
+++ src/main/java/me/kanmodel/july19/onlineteach/adapter/SwaggerConfig.java
@@ -10,6 +10,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * Swagger配置类
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
@@ -17,7 +20,7 @@ public class SwaggerConfig {
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("me.kanmodel.july19.onlineteach.controller.rest"))
+                .apis(RequestHandlerSelectors.basePackage("me.kanmodel.july19.onlineteach.wx.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
